b81960f64a8f1b537e00d06f5562848f147b14dc
chs13010 <christopher.shank@uconn.edu>
2019-07-01 09:18:25 -0400

    Redirect using chrome.tab api. By default the url is highligted so that youcan type a urlto a different tab as well.

--- redirect.js
+++ redirect.js
@@ -1,1 +1,8 @@
-window.location.replace("https://notion.so");
+// Get the current tab
+chrome.tabs.getCurrent(function(tab) {
+  // Update the current tab so it redirects to notion.so and is highlighted
+  chrome.tabs.update(tab.id, {
+    url: "https://notion.so",
+    highlighted: true
+  });
+});
